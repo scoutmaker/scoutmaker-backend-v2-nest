7a96e624fdbce6f7e977cb2d9b76158d5948ff9d
Jakub Figlak <jakub.figlak@netguru.pl>
2022-01-07 14:24:12 +0100

    feat: create register user dto

---
+++ src/auth/dto/register-user.dto.ts
@@ -0,0 +1,48 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { MatchesProperty } from '../decorators/matches-property.decorator';
+
+export class RegisterUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsPhoneNumber()
+  phone?: string;
+
+  @IsString()
+  city?: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, {
+    message:
+      'Password must contain at least 1 lowercase letter, 1 uppercase letter and 1 digit',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(6)
+  @MatchesProperty(RegisterUserDto, (s) => s.password)
+  passwordConfirm: string;
+
+  @IsNumber()
+  @Min(0)
+  activeRadius?: number;
+}
