eddc0e247324fa5a89bd9758902909b5c17829b1
Jakub Figlak <jakub.figlak@netguru.pl>
2022-01-09 18:57:08 +0100

    feat: create a route to fetch all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,9 +7,23 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { formatSuccessResponse } from 'src/utils/helpers';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApiResponse } from '../utils/api-response/api-response.decorator';
+import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @ApiResponse(UserDto, { type: 'read', isArray: true })
+  @Serialize(UserDto)
+  async findAll() {
+    const users = await this.usersService.findAll();
+    return formatSuccessResponse('Successfully fetched all users', users);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+const include: Prisma.UserInclude = { region: { include: { country: true } } };
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.user.findMany({ include });
+  }
 
   findByEmail(email: string) {
     return this.prisma.user.findUnique({
