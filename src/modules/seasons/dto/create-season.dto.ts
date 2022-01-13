22ae38b1469904196c1832743fae17667701f44a
Jakub Figlak <jakub.figlak@netguru.pl>
2022-01-13 11:59:00 +0100

    feat: create seasons dtos

--- src/modules/seasons/dto/create-season.dto.ts
+++ src/modules/seasons/dto/create-season.dto.ts
@@ -1,1 +1,29 @@
-export class CreateSeasonDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateSeasonDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(20)
+  @Transform(({ value }) => value.trim())
+  name: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive = false;
+
+  @IsNotEmpty()
+  @IsDateString()
+  startDate: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  endDate: string;
+}
