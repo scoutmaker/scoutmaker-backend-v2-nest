2b644b55cd742148d5cb2c68ca16437a9222afd2
jakubfiglak <jakub.figlak@netguru.pl>
2022-02-28 23:16:15 +0100

    feat: generate resources for matches

---
+++ src/modules/matches/matches.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { CreateMatchDto } from './dto/create-match.dto';
+import { UpdateMatchDto } from './dto/update-match.dto';
+import { MatchesService } from './matches.service';
+
+@Controller('matches')
+export class MatchesController {
+  constructor(private readonly matchesService: MatchesService) {}
+
+  @Post()
+  create(@Body() createMatchDto: CreateMatchDto) {
+    return this.matchesService.create(createMatchDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.matchesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.matchesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMatchDto: UpdateMatchDto) {
+    return this.matchesService.update(id, updateMatchDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.matchesService.remove(id);
+  }
+}
