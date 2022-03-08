bc2c9ded9f64c67c58301d75e415e86f9190e4eb
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-08 11:40:56 +0100

    feat: add seed functions

---
+++ prisma/seed/report-skill-assessment-categories.ts
@@ -0,0 +1,28 @@
+import { prisma } from './client';
+
+const categoryNames = [
+  'individual',
+  'teamplay',
+  'offense',
+  'defense',
+  'physical',
+  'mental',
+];
+
+export async function generateReportSkillAssessmentCategories(
+  authorId: string,
+) {
+  const promiseArr = categoryNames.map((name) =>
+    prisma.reportSkillAssessmentCategory.create({
+      data: {
+        name,
+        author: { connect: { id: authorId } },
+      },
+    }),
+  );
+
+  const [individual, teamplay, offense, defense, physical, mental] =
+    await Promise.all(promiseArr);
+
+  return { individual, teamplay, offense, defense, physical, mental };
+}
