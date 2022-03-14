1a0dc088baa1fd07be3f7321ce7d125bd255edfc
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-14 09:49:12 +0100

    feat: create follow teams routes

--- src/modules/follow-teams/follow-teams.controller.ts
+++ src/modules/follow-teams/follow-teams.controller.ts
@@ -6,21 +6,51 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
 
+import { ApiResponse } from '../../api-response/api-response.decorator';
+import { AuthGuard } from '../../guards/auth.guard';
+import { Serialize } from '../../interceptors/serialize.interceptor';
+import { formatSuccessResponse } from '../../utils/helpers';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { CurrentUserDto } from '../users/dto/current-user.dto';
+import { CreateFollowTeamDto } from './dto/create-follow-team.dto';
+import { FollowTeamDto } from './dto/follow-team.dto';
 import { FollowTeamsService } from './follow-teams.service';
 
 @Controller('follow-teams')
+@ApiTags('follow teams')
+@UseGuards(AuthGuard)
+@ApiCookieAuth()
+@Serialize(FollowTeamDto)
 export class FollowTeamsController {
   constructor(private readonly followTeamsService: FollowTeamsService) {}
 
-  @Post()
-  create() {
-    return this.followTeamsService.create();
+  @Post(':teamId')
+  @ApiResponse(FollowTeamDto, { type: 'create' })
+  async create(
+    @Param() { teamId }: CreateFollowTeamDto,
+    @CurrentUser() user: CurrentUserDto,
+  ) {
+    const follow = await this.followTeamsService.create(teamId, user.id);
+    return formatSuccessResponse(
+      `Successfully started following team with the id of ${teamId}`,
+      follow,
+    );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.followTeamsService.remove(+id);
+  @Delete(':teamId')
+  @ApiResponse(FollowTeamDto, { type: 'delete' })
+  async remove(
+    @Param() { teamId }: CreateFollowTeamDto,
+    @CurrentUser() user: CurrentUserDto,
+  ) {
+    const follow = await this.followTeamsService.remove(teamId, user.id);
+    return formatSuccessResponse(
+      `Successfully stopped following team with the id of ${teamId}`,
+      follow,
+    );
   }
 }
--- src/modules/follow-teams/follow-teams.service.ts
+++ src/modules/follow-teams/follow-teams.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
+import { PrismaService } from '../prisma/prisma.service';
+
+const include: Prisma.FollowTeamInclude = {
+  follower: true,
+  team: true,
+};
 @Injectable()
 export class FollowTeamsService {
-  create() {
-    return 'This action adds a new followTeam';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(teamId: string, userId: string) {
+    return this.prisma.followTeam.create({
+      data: {
+        team: { connect: { id: teamId } },
+        follower: { connect: { id: userId } },
+      },
+      include,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} followTeam`;
+  remove(teamId: string, userId: string) {
+    return this.prisma.followTeam.delete({
+      where: {
+        teamId_followerId: { teamId, followerId: userId },
+      },
+      include,
+    });
   }
 }
