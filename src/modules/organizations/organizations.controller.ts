68a9cdf7fdfdcf684a2df1370007e4ae442e4587
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-18 14:52:33 +0100

    feat: create organizations controller

--- src/modules/organizations/organizations.controller.ts
+++ src/modules/organizations/organizations.controller.ts
@@ -6,58 +6,116 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
 
+import { ApiResponse } from '../../api-response/api-response.decorator';
+import { AuthGuard } from '../../guards/auth.guard';
+import { RoleGuard } from '../../guards/role.guard';
+import { Serialize } from '../../interceptors/serialize.interceptor';
+import { formatSuccessResponse } from '../../utils/helpers';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { OrganizationDto } from './dto/organization.dto';
 import { ToggleMembershipDto } from './dto/toggle-membership.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
 import { OrganizationsService } from './organizations.service';
 
 @Controller('organizations')
+@ApiTags('organizations')
+@UseGuards(AuthGuard, new RoleGuard(['ADMIN']))
+@ApiCookieAuth()
+@Serialize(OrganizationDto)
 export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
 
   @Post()
-  create(@Body() createOrganizationDto: CreateOrganizationDto) {
-    return this.organizationsService.create(createOrganizationDto);
+  @ApiResponse(OrganizationDto, { type: 'create' })
+  async create(@Body() createOrganizationDto: CreateOrganizationDto) {
+    const organization = await this.organizationsService.create(
+      createOrganizationDto,
+    );
+    return formatSuccessResponse(
+      'Successfully created new organization',
+      organization,
+    );
   }
 
   @Get()
-  findAll() {
-    return this.organizationsService.findAll();
+  @ApiResponse(OrganizationDto, { type: 'read' })
+  async findAll() {
+    const organizations = await this.organizationsService.findAll();
+    return formatSuccessResponse(
+      'Successfully fetched all organizations',
+      organizations,
+    );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.organizationsService.findOne(id);
+  @ApiResponse(OrganizationDto, { type: 'read' })
+  async findOne(@Param('id') id: string) {
+    const organization = await this.organizationsService.findOne(id);
+    return formatSuccessResponse(
+      `Successfully fetched organization ${id}`,
+      organization,
+    );
   }
 
   @Patch(':id')
-  update(
+  @ApiResponse(OrganizationDto, { type: 'update' })
+  async update(
     @Param('id') id: string,
-    updateOrganizationDto: UpdateOrganizationDto,
+    @Body() updateOrganizationDto: UpdateOrganizationDto,
   ) {
-    return this.organizationsService.update(id, updateOrganizationDto);
+    const organization = await this.organizationsService.update(
+      id,
+      updateOrganizationDto,
+    );
+    return formatSuccessResponse(
+      `Successfully updated organization ${id}`,
+      organization,
+    );
   }
 
   @Patch(':id/add-member')
-  addMember(
+  @ApiResponse(OrganizationDto, { type: 'update' })
+  async addMember(
     @Param('id') id: string,
     @Body() toggleMembershipDto: ToggleMembershipDto,
   ) {
-    return this.organizationsService.addMember(id, toggleMembershipDto);
+    const organization = await this.organizationsService.addMember(
+      id,
+      toggleMembershipDto,
+    );
+    return formatSuccessResponse(
+      `Successfully added member ${toggleMembershipDto.memberId} to organization ${id}`,
+      organization,
+    );
   }
 
   @Patch(':id/remove-member')
-  removeMember(
+  @ApiResponse(OrganizationDto, { type: 'update' })
+  async removeMember(
     @Param('id') id: string,
     @Body() toggleMembershipDto: ToggleMembershipDto,
   ) {
-    return this.organizationsService.removeMember(id, toggleMembershipDto);
+    const organization = await this.organizationsService.removeMember(
+      id,
+      toggleMembershipDto,
+    );
+    return formatSuccessResponse(
+      `Successfully removed member ${toggleMembershipDto.memberId} from organization ${id}`,
+      organization,
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.organizationsService.remove(id);
+  @ApiResponse(OrganizationDto, { type: 'delete' })
+  async remove(@Param('id') id: string) {
+    const organization = await this.organizationsService.remove(id);
+    return formatSuccessResponse(
+      `Successfully removed organization ${id}`,
+      organization,
+    );
   }
 }
