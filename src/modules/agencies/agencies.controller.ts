7082e90fdce0010a3721b4aaf5397a76d6b7fc49
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-20 11:16:35 +0100

    feat: add translations for agencies module

--- src/modules/agencies/agencies.controller.ts
+++ src/modules/agencies/agencies.controller.ts
@@ -10,6 +10,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiCookieAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { I18nLang, I18nService } from 'nestjs-i18n';
 
 import { ApiResponse } from '../../api-response/api-response.decorator';
 import { AuthGuard } from '../../guards/auth.guard';
@@ -30,17 +31,25 @@ import { UpdateAgencyDto } from './dto/update-agency.dto';
 @UseGuards(AuthGuard)
 @ApiCookieAuth()
 export class AgenciesController {
-  constructor(private readonly agenciesService: AgenciesService) {}
+  constructor(
+    private readonly agenciesService: AgenciesService,
+    private readonly i18n: I18nService,
+  ) {}
 
   @Post()
   @ApiResponse(AgencyDto, { type: 'create' })
   @Serialize(AgencyDto)
   async create(
+    @I18nLang() lang: string,
     @Body() createAgencyDto: CreateAgencyDto,
     @CurrentUser() user: CurrentUserDto,
   ) {
     const agency = await this.agenciesService.create(createAgencyDto, user.id);
-    return formatSuccessResponse('Successfully created new agency', agency);
+    const message = await this.i18n.translate('agencies.CREATE_MESSAGE', {
+      lang,
+      args: { name: agency.name },
+    });
+    return formatSuccessResponse(message, agency);
   }
 
   @Get()
@@ -48,48 +57,66 @@ export class AgenciesController {
   @ApiQuery({ type: AgenciesPaginationOptions })
   @Serialize(AgencyDto, 'docs')
   async findAll(
+    @I18nLang() lang: string,
     @PaginationOptions() paginationOptions: AgenciesPaginationOptions,
     @Query() query: FindAllAgenciesDto,
   ) {
     const data = await this.agenciesService.findAll(paginationOptions, query);
-    return formatSuccessResponse('Successfully fetched all agencies', data);
+    const message = await this.i18n.translate('agencies.GET_ALL_MESSAGE', {
+      lang,
+      args: { currentPage: data.page, totalPages: data.totalPages },
+    });
+    return formatSuccessResponse(message, data);
   }
 
   @Get('list')
   @ApiResponse(AgencyBasicInfoDto, { type: 'read' })
   @Serialize(AgencyBasicInfoDto)
-  async getList() {
-    const agency = await this.agenciesService.getList();
-    return formatSuccessResponse(
-      'Successfully fetched all agencies list',
-      agency,
-    );
+  async getList(@I18nLang() lang: string) {
+    const agencies = await this.agenciesService.getList();
+    const message = await this.i18n.translate('agencies.GET_LIST_MESSAGE', {
+      lang,
+    });
+    return formatSuccessResponse(message, agencies);
   }
 
   @Get(':id')
   @ApiResponse(AgencyDto, { type: 'read' })
   @Serialize(AgencyDto)
-  async findOne(@Param('id') id: string) {
+  async findOne(@I18nLang() lang: string, @Param('id') id: string) {
     const agency = await this.agenciesService.findOne(id);
-    return formatSuccessResponse(`Successfully fetched #${id} agency`, agency);
+    const message = await this.i18n.translate('agencies.GET_ONE_MESSAGE', {
+      lang,
+      args: { name: agency.name },
+    });
+    return formatSuccessResponse(message, agency);
   }
 
   @Patch(':id')
   @ApiResponse(AgencyDto, { type: 'update' })
   @Serialize(AgencyDto)
   async update(
+    @I18nLang() lang: string,
     @Param('id') id: string,
     @Body() updateAgencyDto: UpdateAgencyDto,
   ) {
     const agency = await this.agenciesService.update(id, updateAgencyDto);
-    return formatSuccessResponse(`Successfully updated #${id} agency`, agency);
+    const message = await this.i18n.translate('agencies.UPDATE_MESSAGE', {
+      lang,
+      args: { name: agency.name },
+    });
+    return formatSuccessResponse(message, agency);
   }
 
   @Delete(':id')
   @ApiResponse(AgencyDto, { type: 'delete' })
   @Serialize(AgencyDto)
-  async remove(@Param('id') id: string) {
+  async remove(@I18nLang() lang: string, @Param('id') id: string) {
     const agency = await this.agenciesService.remove(id);
-    return formatSuccessResponse(`Successfully removed #${id} agency`, agency);
+    const message = await this.i18n.translate('agencies.DELETE_MESSAGE', {
+      lang,
+      args: { name: agency.name },
+    });
+    return formatSuccessResponse(message, agency);
   }
 }
