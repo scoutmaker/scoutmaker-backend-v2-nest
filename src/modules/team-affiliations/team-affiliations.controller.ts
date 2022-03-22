069a3a230b7f7b14fe9251ba6d3875db416592ca
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-22 09:10:23 +0100

    feat: add translations for team-affiliations module

--- src/modules/team-affiliations/team-affiliations.controller.ts
+++ src/modules/team-affiliations/team-affiliations.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
+import { I18nLang, I18nService } from 'nestjs-i18n';
 
 import { ApiResponse } from '../../api-response/api-response.decorator';
 import { AuthGuard } from '../../guards/auth.guard';
@@ -27,43 +28,63 @@ import { TeamAffiliationsService } from './team-affiliations.service';
 export class TeamAffiliationsController {
   constructor(
     private readonly teamAffiliationsService: TeamAffiliationsService,
+    private readonly i18n: I18nService,
   ) {}
 
   @Post()
   @ApiResponse(TeamAffiliationDto, { type: 'create' })
-  async create(@Body() createTeamAffiliationDto: CreateTeamAffiliationDto) {
+  async create(
+    @I18nLang() lang: string,
+    @Body() createTeamAffiliationDto: CreateTeamAffiliationDto,
+  ) {
     const affiliation = await this.teamAffiliationsService.create(
       createTeamAffiliationDto,
     );
-    return formatSuccessResponse(
-      'Successfully created new team affiliation',
-      affiliation,
+    const message = await this.i18n.translate(
+      'team-affiliations.CREATE_MESSAGE',
+      {
+        lang,
+        args: {
+          playerName: `${affiliation.player.firstName} ${affiliation.player.lastName}`,
+          teamName: `${affiliation.team.name}`,
+        },
+      },
     );
+    return formatSuccessResponse(message, affiliation);
   }
 
   @Get()
   @ApiResponse(TeamAffiliationDto, { type: 'read' })
-  async findAll() {
+  async findAll(@I18nLang() lang: string) {
     const affiliations = await this.teamAffiliationsService.findAll();
-    return formatSuccessResponse(
-      'Successfully fetched all team affiliations',
-      affiliations,
+    const message = await this.i18n.translate(
+      'team-affiliations.GET_LIST_MESSAGE',
+      { lang },
     );
+    return formatSuccessResponse(message, affiliations);
   }
 
   @Get(':id')
   @ApiResponse(TeamAffiliationDto, { type: 'read' })
-  async findOne(@Param('id') id: string) {
+  async findOne(@I18nLang() lang: string, @Param('id') id: string) {
     const affiliation = await this.teamAffiliationsService.findOne(id);
-    return formatSuccessResponse(
-      `Successfully fetched team affiliation with the id of ${id}`,
-      affiliation,
+    const message = await this.i18n.translate(
+      'team-affiliations.GET_ONE_MESSAGE',
+      {
+        lang,
+        args: {
+          playerName: `${affiliation.player.firstName} ${affiliation.player.lastName}`,
+          teamName: `${affiliation.team.name}`,
+        },
+      },
     );
+    return formatSuccessResponse(message, affiliation);
   }
 
   @Patch(':id')
   @ApiResponse(TeamAffiliationDto, { type: 'update' })
   async update(
+    @I18nLang() lang: string,
     @Param('id') id: string,
     @Body() updateTeamAffiliationDto: UpdateTeamAffiliationDto,
   ) {
@@ -71,19 +92,33 @@ export class TeamAffiliationsController {
       id,
       updateTeamAffiliationDto,
     );
-    return formatSuccessResponse(
-      `Successfully updated team affiliation with the id of ${id}`,
-      affiliation,
+    const message = await this.i18n.translate(
+      'team-affiliations.UPDATE_MESSAGE',
+      {
+        lang,
+        args: {
+          playerName: `${affiliation.player.firstName} ${affiliation.player.lastName}`,
+          teamName: `${affiliation.team.name}`,
+        },
+      },
     );
+    return formatSuccessResponse(message, affiliation);
   }
 
   @Delete(':id')
   @ApiResponse(TeamAffiliationDto, { type: 'delete' })
-  async remove(@Param('id') id: string) {
+  async remove(@I18nLang() lang: string, @Param('id') id: string) {
     const affiliation = await this.teamAffiliationsService.remove(id);
-    return formatSuccessResponse(
-      `Successfully removed team affiliation with the id of ${id}`,
-      affiliation,
+    const message = await this.i18n.translate(
+      'team-affiliations.DELETE_MESSAGE',
+      {
+        lang,
+        args: {
+          playerName: `${affiliation.player.firstName} ${affiliation.player.lastName}`,
+          teamName: `${affiliation.team.name}`,
+        },
+      },
     );
+    return formatSuccessResponse(message, affiliation);
   }
 }
