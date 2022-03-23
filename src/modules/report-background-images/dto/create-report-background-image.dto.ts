cbf8744cc480fcd15eed910a01e084f1256abc96
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-23 21:21:43 +0100

    feat: create report background image dtos

--- src/modules/report-background-images/dto/create-report-background-image.dto.ts
+++ src/modules/report-background-images/dto/create-report-background-image.dto.ts
@@ -1,1 +1,15 @@
-export class CreateReportBackgroundImageDto {}
+import { IsBoolean, IsOptional, IsUrl } from 'class-validator';
+
+import { IsRequiredStringWithMaxLength } from '../../../decorators/is-required-string-with-max-length.decorator';
+
+export class CreateReportBackgroundImageDto {
+  @IsRequiredStringWithMaxLength(20)
+  name: string;
+
+  @IsUrl()
+  url: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPublic: boolean;
+}
