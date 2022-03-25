353324afd22f866a01ca5b1bcc40b441cd1d3419
jakubfiglak <jakub.figlak@netguru.pl>
2022-03-25 20:13:30 +0100

    feat: create service methods for user subscriptions module

--- src/modules/user-subscriptions/user-subscriptions.service.ts
+++ src/modules/user-subscriptions/user-subscriptions.service.ts
@@ -1,26 +1,116 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserSubscriptionDto } from './dto/create-user-subscription.dto';
 import { UpdateUserSubscriptionDto } from './dto/update-user-subscription.dto';
 
+const include = Prisma.validator<Prisma.UserSubscriptionInclude>()({
+  user: true,
+  competitions: { include: { competition: true } },
+  competitionGroups: { include: { group: true } },
+});
+
 @Injectable()
 export class UserSubscriptionsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserSubscriptionDto: CreateUserSubscriptionDto) {
-    return 'This action adds a new userSubscription';
+    const { competitionIds, competitionGroupIds, userId, startDate, endDate } =
+      createUserSubscriptionDto;
+
+    return this.prisma.userSubscription.create({
+      data: {
+        user: { connect: { id: userId } },
+        startDate: new Date(startDate),
+        endDate: new Date(endDate),
+        competitions: {
+          createMany: {
+            data: competitionIds.map((id) => ({ competitionId: id })),
+          },
+        },
+        competitionGroups:
+          competitionGroupIds && competitionGroupIds.length > 0
+            ? {
+                createMany: {
+                  data: competitionGroupIds.map((id) => ({
+                    groupId: id,
+                  })),
+                },
+              }
+            : undefined,
+      },
+      include,
+    });
   }
 
   findAll() {
-    return `This action returns all userSubscriptions`;
+    return this.prisma.userSubscription.findMany({ include });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} userSubscription`;
+  findOne(id: string) {
+    return this.prisma.userSubscription.findUnique({ where: { id }, include });
   }
 
-  update(id: number, updateUserSubscriptionDto: UpdateUserSubscriptionDto) {
-    return `This action updates a #${id} userSubscription`;
+  async update(
+    id: string,
+    updateUserSubscriptionDto: UpdateUserSubscriptionDto,
+  ) {
+    const { startDate, endDate, competitionIds, competitionGroupIds } =
+      updateUserSubscriptionDto;
+
+    const shouldUpdateCompetitions =
+      competitionIds && competitionIds.length > 0;
+    const shouldUpdateCompetitionGroups =
+      competitionGroupIds && competitionGroupIds.length > 0;
+
+    if (shouldUpdateCompetitions) {
+      await this.prisma.competitionsOnUserSubscriptions.deleteMany({
+        where: { subscriptionId: id },
+      });
+    }
+
+    if (shouldUpdateCompetitionGroups) {
+      await this.prisma.competitionGroupsOnUserSubscriptions.deleteMany({
+        where: { subscriptionId: id },
+      });
+    }
+
+    return this.prisma.userSubscription.update({
+      where: { id },
+      data: {
+        startDate: startDate ? new Date(startDate) : undefined,
+        endDate: endDate ? new Date(endDate) : undefined,
+        competitions: shouldUpdateCompetitions
+          ? {
+              createMany: {
+                data: competitionIds.map((id) => ({
+                  competitionId: id,
+                })),
+              },
+            }
+          : undefined,
+        competitionGroups: shouldUpdateCompetitionGroups
+          ? {
+              createMany: {
+                data: competitionGroupIds.map((id) => ({
+                  groupId: id,
+                })),
+              },
+            }
+          : undefined,
+      },
+      include,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} userSubscription`;
+  async remove(id: string) {
+    await this.prisma.competitionsOnUserSubscriptions.deleteMany({
+      where: { subscriptionId: id },
+    });
+    await this.prisma.competitionGroupsOnUserSubscriptions.deleteMany({
+      where: { subscriptionId: id },
+    });
+    return this.prisma.userSubscription.delete({ where: { id }, include });
   }
 }
