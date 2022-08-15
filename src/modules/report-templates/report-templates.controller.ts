b1861bd5622df7a573001b4b0897d0d24904f9d1
jakubfiglak <jakub.figlak@netguru.pl>
2022-08-15 14:56:26 +0200

    feat: create GET / endpoint for report templates module

--- src/modules/report-templates/report-templates.controller.ts
+++ src/modules/report-templates/report-templates.controller.ts
@@ -7,19 +7,24 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
+import { ApiCookieAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { I18nLang, I18nService } from 'nestjs-i18n';
 
+import { ApiPaginatedResponse } from '../../common/api-response/api-paginated-response.decorator';
 import { ApiResponse } from '../../common/api-response/api-response.decorator';
 import { AuthGuard } from '../../common/guards/auth.guard';
 import { Serialize } from '../../common/interceptors/serialize.interceptor';
+import { PaginationOptions } from '../../common/pagination/pagination-options.decorator';
 import { formatSuccessResponse } from '../../utils/helpers';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { CurrentUserDto } from '../users/dto/current-user.dto';
 import { CreateReportTemplateDto } from './dto/create-report-template.dto';
+import { FindAllReportTemplatesDto } from './dto/find-all-report-templates.dto';
 import { ReportTemplateDto } from './dto/report-template.dto';
+import { ReportTemplatesPaginationOptionsDto } from './dto/report-templates-pagination-options.dto';
 import { UpdateReportTemplateDto } from './dto/update-report-template.dto';
 import { ReportTemplatesService } from './report-templates.service';
 
@@ -27,7 +32,6 @@ import { ReportTemplatesService } from './report-templates.service';
 @ApiTags('report templates')
 @UseGuards(AuthGuard)
 @ApiCookieAuth()
-@Serialize(ReportTemplateDto)
 export class ReportTemplatesController {
   constructor(
     private readonly reportTemplatesService: ReportTemplatesService,
@@ -36,6 +40,7 @@ export class ReportTemplatesController {
 
   @Post()
   @ApiResponse(ReportTemplateDto, { type: 'create' })
+  @Serialize(ReportTemplateDto)
   async create(
     @I18nLang() lang: string,
     @Body() createReportTemplateDto: CreateReportTemplateDto,
@@ -53,9 +58,31 @@ export class ReportTemplatesController {
   }
 
   @Get()
+  @ApiPaginatedResponse(ReportTemplateDto)
+  @ApiQuery({ type: ReportTemplatesPaginationOptionsDto })
+  @Serialize(ReportTemplateDto, 'docs')
+  async findAll(
+    @I18nLang() lang: string,
+    @PaginationOptions()
+    paginationOptions: ReportTemplatesPaginationOptionsDto,
+    @Query() query: FindAllReportTemplatesDto,
+  ) {
+    const data = await this.reportTemplatesService.findAll(
+      paginationOptions,
+      query,
+    );
+    const message = this.i18n.translate('report-templates.GET_ALL_MESSAGE', {
+      lang,
+      args: { currentPage: data.page, totalPages: data.totalPages },
+    });
+    return formatSuccessResponse(message, data);
+  }
+
+  @Get('list')
   @ApiResponse(ReportTemplateDto, { type: 'read' })
-  async findAll(@I18nLang() lang: string) {
-    const templates = await this.reportTemplatesService.findAll();
+  @Serialize(ReportTemplateDto)
+  async getList(@I18nLang() lang: string) {
+    const templates = await this.reportTemplatesService.getList();
     const message = this.i18n.translate('report-templates.GET_LIST_MESSAGE', {
       lang,
     });
@@ -64,6 +91,7 @@ export class ReportTemplatesController {
 
   @Get(':id')
   @ApiResponse(ReportTemplateDto, { type: 'read' })
+  @Serialize(ReportTemplateDto)
   async findOne(
     @I18nLang() lang: string,
     @Param('id', ParseIntPipe) id: number,
@@ -78,6 +106,7 @@ export class ReportTemplatesController {
 
   @Patch(':id')
   @ApiResponse(ReportTemplateDto, { type: 'update' })
+  @Serialize(ReportTemplateDto)
   async update(
     @I18nLang() lang: string,
     @Param('id', ParseIntPipe) id: number,
@@ -96,6 +125,7 @@ export class ReportTemplatesController {
 
   @Delete(':id')
   @ApiResponse(ReportTemplateDto, { type: 'delete' })
+  @Serialize(ReportTemplateDto)
   async remove(
     @I18nLang() lang: string,
     @Param('id', ParseIntPipe) id: number,
