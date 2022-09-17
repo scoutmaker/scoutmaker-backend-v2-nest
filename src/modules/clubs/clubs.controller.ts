47c14ce0f0e2fdadf95c915d8ef60bd18ea1c007
jakubfiglak <jakub.figlak@netguru.pl>
2022-09-17 16:40:18 +0200

    feat: add helpers & function to create multiple clubs from csv

--- src/modules/clubs/clubs.controller.ts
+++ src/modules/clubs/clubs.controller.ts
@@ -4,18 +4,27 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
   Patch,
   Post,
   Query,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
-import { ApiCookieAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiCookieAuth,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { I18nLang, I18nService } from 'nestjs-i18n';
 
 import { ApiPaginatedResponse } from '../../common/api-response/api-paginated-response.decorator';
 import { ApiResponse } from '../../common/api-response/api-response.decorator';
 import { AuthGuard } from '../../common/guards/auth.guard';
+import { RoleGuard } from '../../common/guards/role.guard';
 import { Serialize } from '../../common/interceptors/serialize.interceptor';
 import { PaginationOptions } from '../../common/pagination/pagination-options.decorator';
 import { formatSuccessResponse } from '../../utils/helpers';
@@ -52,6 +61,31 @@ export class ClubsController {
       args: { name: club.name },
     });
     return formatSuccessResponse(message, club);
+  }
+
+  @Post('upload')
+  @UseGuards(new RoleGuard(['ADMIN']))
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const { createdDocuments, errors } =
+      await this.clubsService.createManyFromCsv(file);
+    return formatSuccessResponse('Success!', {
+      createdCount: createdDocuments.length,
+      createdDocuments,
+      errors,
+    });
   }
 
   @Get()
