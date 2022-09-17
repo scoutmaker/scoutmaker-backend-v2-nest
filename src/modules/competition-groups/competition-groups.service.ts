408b19ad38fe0f7faf2c8bb4c1a590b4e345268c
jakubfiglak <jakub.figlak@netguru.pl>
2022-09-17 18:49:33 +0200

    feat: add method for competition groups

--- src/modules/competition-groups/competition-groups.service.ts
+++ src/modules/competition-groups/competition-groups.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { CompetitionType, Prisma } from '@prisma/client';
 
+import { parseCsv, validateInstances } from '../../utils/csv-helpers';
 import {
   calculateSkip,
   formatPaginatedResponse,
@@ -16,6 +17,14 @@ const include: Prisma.CompetitionGroupInclude = {
   competition: { include: { country: true } },
   regions: { include: { region: true } },
 };
+
+interface CsvInput {
+  id: number;
+  name: string;
+  transfermarktUrl?: string;
+  competitionId: number;
+  regionIds?: string;
+}
 
 @Injectable()
 export class CompetitionGroupsService {
@@ -35,6 +44,42 @@ export class CompetitionGroupsService {
       },
       include,
     });
+  }
+
+  async createManyFromCsv(file: Express.Multer.File) {
+    const result = parseCsv<CsvInput>(file.buffer.toString());
+
+    const instances = result.data.map((item) => {
+      const instance = new CreateCompetitionGroupDto();
+
+      instance.id = item.id?.toString();
+      instance.name = item.name;
+      instance.transfermarktUrl = item.transfermarktUrl;
+      instance.competitionId = item.competitionId?.toString();
+      instance.regionIds = item.regionIds?.split(',');
+
+      return instance;
+    });
+
+    await validateInstances(instances);
+
+    const createdDocuments: CompetitionType[] = [];
+    const errors: any[] = [];
+
+    for (const [index, instance] of instances.entries()) {
+      try {
+        const created = await this.create(instance);
+        createdDocuments.push(created);
+      } catch (error) {
+        errors.push({ index, name: instance.name, error });
+      }
+    }
+
+    return {
+      csvRowsCount: result.data.length,
+      createdCount: createdDocuments.length,
+      errors,
+    };
   }
 
   async findAll(
