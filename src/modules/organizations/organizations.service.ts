08756adfd8256831352ae46216d13337887097b1
jakubfiglak <jakub.figlak@netguru.pl>
2022-10-02 16:39:38 +0200

    feat: remove organization memeber role constraint

--- src/modules/organizations/organizations.service.ts
+++ src/modules/organizations/organizations.service.ts
@@ -34,18 +34,7 @@ export class OrganizationsService {
       where: { id: { in: memberIds } },
     });
 
-    // 1. Check if members are valid (only regular scouts can be organization members)
-    const notScoutUsers = members.filter((user) => user.role !== 'SCOUT');
-
-    if (notScoutUsers.length !== 0) {
-      const message = this.i18n.translate(
-        'organizations.CREATE_MEMBERS_ROLE_ERROR',
-        { lang, args: { userNames: getUserNamesString(notScoutUsers) } },
-      );
-      throw new BadRequestException(message);
-    }
-
-    // 2. Check if members are not already in an organization
+    // Check if members are not already in an organization
     const alreadyInOrganizationUsers = members.filter(
       (user) => user.organizationId,
     );
@@ -153,15 +142,7 @@ export class OrganizationsService {
 
     const name = `${member.firstName} ${member.lastName}`;
 
-    // 1. Check if member is valid (only regular scouts can be members of an organization)
-    if (member.role !== 'SCOUT') {
-      const message = this.i18n.translate(
-        'organizations.ADD_MEMBER_ROLE_ERROR',
-        { lang, args: { userName: name, role: member.role } },
-      );
-      throw new BadRequestException(message);
-    }
-    // 2. Check if member is not already in an organization
+    // Check if member is not already in an organization
     if (member.organizationId) {
       const message = this.i18n.translate(
         'organizations.ADD_MEMBER_ALREADY_IN_ORG_ERROR',
