179feb4ae84b10e8cb6ebaad86778a921b2ba228
dejwi <misti.krasula@gmail.com>
2022-11-12 12:02:56 +0100

    fix typo

--- src/modules/matches/interceptors/access-filters.interceptor.ts
+++ src/modules/matches/interceptors/access-filters.interceptor.ts
@@ -80,7 +80,7 @@ export class AccessFiltersInterceptor implements NestInterceptor {
       organizationSubscriptions,
     );
 
-    const playmakerExtraAccess: Prisma.PlayerWhereInput =
+    const playmakerExtraAccess: Prisma.MatchWhereInput =
       user.role === 'PLAYMAKER_SCOUT_MANAGER'
         ? { author: { role: { not: 'SCOUT' } } }
         : {};
