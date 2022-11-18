7ead7bd3959881df2e2fe37ff701723ef1a0fd35
dejwi <misti.krasula@gmail.com>
2022-11-18 15:33:04 +0100

    include organizationId

--- src/modules/organization-subscriptions/dto/update-organization-subscription.dto.ts
+++ src/modules/organization-subscriptions/dto/update-organization-subscription.dto.ts
@@ -1,7 +1,3 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-
 import { CreateOrganizationSubscriptionDto } from './create-organization-subscription.dto';
 
-export class UpdateOrganizationSubscriptionDto extends PartialType(
-  OmitType(CreateOrganizationSubscriptionDto, ['organizationId']),
-) {}
+export class UpdateOrganizationSubscriptionDto extends CreateOrganizationSubscriptionDto {}
