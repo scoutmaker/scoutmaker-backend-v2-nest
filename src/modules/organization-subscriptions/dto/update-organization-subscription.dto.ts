8fa3ea7d619c82ef4ef7feb074e4b0f585b5ec29
dejwi <misti.krasula@gmail.com>
2022-11-19 10:46:12 +0100

    Revert "include organizationId"
    This reverts commit 7ead7bd3959881df2e2fe37ff701723ef1a0fd35.

--- src/modules/organization-subscriptions/dto/update-organization-subscription.dto.ts
+++ src/modules/organization-subscriptions/dto/update-organization-subscription.dto.ts
@@ -1,3 +1,7 @@
+import { OmitType, PartialType } from '@nestjs/swagger';
+
 import { CreateOrganizationSubscriptionDto } from './create-organization-subscription.dto';
 
-export class UpdateOrganizationSubscriptionDto extends CreateOrganizationSubscriptionDto {}
+export class UpdateOrganizationSubscriptionDto extends PartialType(
+  OmitType(CreateOrganizationSubscriptionDto, ['organizationId']),
+) {}
