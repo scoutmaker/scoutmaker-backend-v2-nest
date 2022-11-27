aa7cd951d69e9e4c305cacb2a48ef6e14f85f389
jakubfiglak <jakub.figlak@netguru.pl>
2022-11-27 15:15:52 +0100

    fix: merge observation type migrations

--- prisma/migrations/20221107165642_observation_type1/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "ObservationType" AS ENUM ('LIVE', 'VIDEO');
-
--- AlterTable
-ALTER TABLE "Note" ADD COLUMN     "observationType" "ObservationType";
-
--- AlterTable
-ALTER TABLE "Report" ADD COLUMN     "observationType" "ObservationType";
