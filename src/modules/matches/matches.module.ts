cf0382c99b21fe1cbf647e602cb02bdb71890d41
jakubfiglak <jakub.figlak@netguru.pl>
2022-11-30 18:26:59 +0100

    feat: use AdminOrAuthorGuard in matches service

--- src/modules/matches/matches.module.ts
+++ src/modules/matches/matches.module.ts
@@ -1,12 +1,16 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 
+import { featureServiceName } from '../../common/guards/admin-or-author.guard';
 import { PrepareQueryMiddleware } from '../../common/middleware/prepare-query.middleware';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 
 @Module({
   controllers: [MatchesController],
-  providers: [MatchesService],
+  providers: [
+    MatchesService,
+    { provide: featureServiceName, useExisting: MatchesService },
+  ],
 })
 export class MatchesModule {
   configure(consumer: MiddlewareConsumer) {
