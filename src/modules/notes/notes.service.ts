1269a5f3a67b870015f5f8bcaff637a76b7c4859
jakubfiglak <jakub.figlak@netguru.pl>
2022-12-14 08:40:07 +0100

    fix: optional chaining for meta fields

--- src/modules/notes/notes.service.ts
+++ src/modules/notes/notes.service.ts
@@ -114,9 +114,9 @@ export class NotesService {
       metaPositionId = positionPlayedId || player.primaryPositionId;
       metaTeamId = teamId || player.teams[0]?.teamId;
       metaCompetitionId =
-        competitionId || player.teams[0]?.team.competitions[0].competitionId;
+        competitionId || player.teams[0]?.team.competitions[0]?.competitionId;
       metaCompetitionGroupId =
-        competitionGroupId || player.teams[0]?.team.competitions[0].groupId;
+        competitionGroupId || player.teams[0]?.team.competitions[0]?.groupId;
     }
 
     return this.prisma.note.create({
@@ -152,6 +152,7 @@ export class NotesService {
 
     const instances = result.data.map((item) => {
       const instance = new CreateNoteDto();
+
       instance.id = item.id?.toString();
       instance.shirtNo = item.shirtNo;
       instance.description = item.description;
@@ -421,7 +422,7 @@ export class NotesService {
       const metaCompetitionId =
         competitionId || player.teams[0].team.competitions[0]?.competitionId;
       const metaCompetitionGroupId =
-        competitionGroupId || player.teams[0]?.team.competitions[0].groupId;
+        competitionGroupId || player.teams[0]?.team.competitions[0]?.groupId;
 
       await this.prisma.noteMeta.create({
         data: {
